579d905ecf4b0480ab5065b8a58989f1e0b12049
Avirup <avirup.pal@gmail.com>
2019-02-02 21:59:54 +0530

    package names changes

--- src/main/java/com/orastays/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/orastays/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.stays.eurekaserver;
+package com.orastays.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
